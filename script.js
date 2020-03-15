675ac9c8e92a219932a7d6e93b22e1b2777da182
Polina Barabash <polina_11.92@inbox.ru>
2020-03-15 14:53:23 +0300

    feat: implement image sort by tag in portfolio

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 window.onload = function () {
     switchTabMenu();
+    switchPortfolioTags();
 }
 
 const switchTabMenu = () => {
@@ -16,6 +17,38 @@ const switchTabMenu = () => {
     })
 } 
 
+const switchPortfolioTags = () => {
+    //add click event to portfolio tags
+    const tags = document.querySelector('.portfolio__tags').addEventListener('click', (event) => {
+        //remove class from active tag
+        document.querySelectorAll('span').forEach(el => el.classList.remove('portfolio__tags_selected'));
+        //add class active to selected tag
+        event.target.classList.add('portfolio__tags_selected');
+        let selectedTag = event.target;
+        //image sort
+        sortPortfolioImage(selectedTag);
+    })
+}
+
+const sortPortfolioImage = (selectedTag) => {
+    hiddenAllPortfolioImage();
+    if (selectedTag.innerHTML === 'All') {
+        document.querySelectorAll('.portfolio__gallery-item').forEach(el => el.classList.remove('portfolio__gallery-item_hidden'));
+    }
+    if (selectedTag.innerHTML === 'Web Design') {
+        document.querySelectorAll('.web').forEach(el => el.classList.remove('portfolio__gallery-item_hidden'));
+    }
+    if (selectedTag.innerHTML === 'Graphic Design') {
+        document.querySelectorAll('.graphic').forEach(el => el.classList.remove('portfolio__gallery-item_hidden'));
+    }
+    if (selectedTag.innerHTML === 'Artwork') {
+        document.querySelectorAll('.artwork').forEach(el => el.classList.remove('portfolio__gallery-item_hidden'));
+    }
+};
+
+const hiddenAllPortfolioImage = () => {
+    document.querySelectorAll('.portfolio__gallery-item').forEach(el => el.classList.add('portfolio__gallery-item_hidden'));
+};
 
 
 
