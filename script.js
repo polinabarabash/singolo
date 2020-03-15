ba3d54c66f31e458e00750e077dcd0354e8c8d14
Polina Barabash <polina_11.92@inbox.ru>
2020-03-15 19:13:44 +0300

    feat: add modal window

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ window.onload = function () {
     switchTabMenu();
     switchPortfolioTags();
     borderedPortfolioImage();
+    openModalForm();
+    closeModalForm();    
 }
 
 //---navigation---
@@ -58,10 +60,63 @@ const borderedPortfolioImage = () => {
     const img = document.querySelector('.portfolio__gallery').addEventListener('click', (event) => {
         //remove class bordered for another image
         document.querySelectorAll('.portfolio__gallery-item img').forEach(el => el.classList.remove('portfolio__gallery-item_bordered'));
-        //add class bordered for selected image
-        console.log(event.target);
+        //add class bordered for selected image        
         event.target.classList.add('portfolio__gallery-item_bordered');
     })
 }
 
+//---form---
+const openModalForm = () => {
+    //add click event for submit button
+    const formButton = document.querySelector('.form-button').addEventListener('click', (event) => {
+        //block default post from form
+        event.preventDefault();
+        //open modal form
+        document.querySelector('.getaquote__modal-window').classList.remove('getaquote__modal-window_hidden');
+        getDataFromForm();
+    })
+}
+
+const closeModalForm = () => {
+    //add click event for modal button
+    const formButton = document.querySelector('.getaquote__modal-button').addEventListener('click', (event) => {        
+        //close modal form
+        document.querySelector('.getaquote__modal-window').classList.add('getaquote__modal-window_hidden');        
+        //clean modal windiw 
+        let elemP = document.querySelectorAll('.getaquote__modal p').forEach(el => {
+            document.querySelector('.getaquote__modal').removeChild(el);  
+        });        
+             
+    })
+}
+
+const getDataFromForm = () => {
+    //subject
+    const subject = document.querySelector('input[name="subject"]');    
+    if (subject.value === 'Singolo' || subject.value === 'singolo') {
+        let p = document.createElement('p');
+        let topic = document.createTextNode('Тема: ' + subject.value);
+        p.appendChild(topic);
+        document.querySelector('.getaquote__modal').appendChild(p);           
+    } else {
+        let p = document.createElement('p');
+        let topicNone = document.createTextNode('Без темы');
+        p.appendChild(topicNone);
+        document.querySelector('.getaquote__modal').appendChild(p);
+    } 
+    //description
+    const description = document.querySelector('textarea[name="description-project"]');    
+    if (description.value === 'Portfolio project' || description.value === 'portfolio project') {       
+        let p = document.createElement('p');
+        let descriptionProject = document.createTextNode('Описание: ' + description.value);      
+        p.appendChild(descriptionProject);
+        document.querySelector('.getaquote__modal').appendChild(p);                 
+    } else {        
+        let p = document.createElement('p');
+        let descriptionProjectNone = document.createTextNode('Без описания'); 
+        p.appendChild(descriptionProjectNone);     
+        document.querySelector('.getaquote__modal').appendChild(p);
+    }          
+    
+}
 
